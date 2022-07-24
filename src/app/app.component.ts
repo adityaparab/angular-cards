76c81df83b6c3b4d9be19ac3552c2e46dc285a9c
Aditya Parab <contact.adityaparab@gmail.com>
2022-07-24 19:05:06 +0200

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+type Post = {
+  title: string;
+  username: string;
+  imageUrl: string;
+  content: string;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-cards';
+  posts: Post[] = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'Saw this awesome tree during my hike today.',
+    },
+    {
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking1222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CardComponent, NavbarComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
